6ce2835ce2e18b61896f8ab4130a8f7744bd25c3
constcpt <51440152+constcpt@users.noreply.github.com>
2023-08-17 00:10:54 +1000

    Home页NearbyShops列表从静态数据改为从模拟的后端接口动态获取

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,24 @@
 import axios from "axios";
 
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url, {
+        params: params,
+        baseURL:
+          "https://www.fastmock.site/mock/e41bd2452975e01e25da2d8e06708fc1/jd",
+      })
+      .then(
+        (response) => {
+          resolve(response.data);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+  });
+};
+
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
     axios
--- src/views/home/NearbyShops.vue
+++ src/views/home/NearbyShops.vue
@@ -1,57 +1,67 @@
 <template>
     <div class="nearby">
         <h3 class="nearby__title">Nearby Shops</h3>
-        <div
-         class="nearby__shop"
-         v-for="item in nearbyList"
-         :key="item.id"
-         >
+        <div class="nearby__shop" v-for="item in nearbyList" :key="item._id">
             <img :src="item.imgUrl" class="nearby__shop__img">
             <div class="nearby__content">
-                <div class="nearby__content__title">{{ item.title }}</div>
+                <div class="nearby__content__title">{{ item.name }}</div>
                 <div class="nearby__content__tags">
-                    <span
-                        class="nearby__content__tag"
-                        v-for="(innerItem, innerIndex) in item.tags"
-                        :key="innerIndex"
-                    >{{ innerItem }}</span>
+                    <span class="nearby__content__tag">Sales: {{ item.sales }}</span>
+                    <span class="nearby__content__tag">Min. Order: {{ item.expressLimit }}</span>
+                    <span class="nearby__content__tag">Delivery Fee: {{ item.expressPrice }}</span>
                 </div>
-                <p class="nearby__content__highlight">{{ item.desc }}</p>
+                <p class="nearby__content__highlight">{{ item.slogan }}</p>
             </div>
         </div>
     </div>
 </template>
 <script>
+import { ref } from 'vue';
+import { get } from '../../utils/request';
+
+const useNearbyListEffect = () => {
+    const nearbyList = ref([]);
+    const getNearbyList = async () => {
+        const result = await get('/api/shop/hot-list')
+        if (result?.errno === 0 && result?.data?.length) {
+            nearbyList.value = result.data
+        }
+    }
+    return { nearbyList, getNearbyList }
+}
 export default {
-  name: 'NearbyShops',
-  setup () {
-    const nearbyList = [{
-      id: 1,
-      imgUrl: 'https://i.imgur.com/LMzplaf.png',
-      title: 'Coles',
-      tags: ['A', 'B', 'C'],
-      desc: 'Good things are happening at Coles!'
-    }, {
-      id: 2,
-      imgUrl: 'https://i.imgur.com/FPLfdoB.png',
-      title: 'Woolworths',
-      tags: ['A', 'B', 'C'],
-      desc: 'Bring a little good to everyone, everyday.'
-    }, {
-      id: 3,
-      imgUrl: 'https://i.imgur.com/FPLfdoB.png',
-      title: 'Woolworths',
-      tags: ['A', 'B', 'C'],
-      desc: 'Bring a little good to everyone, everyday.'
-    }, {
-      id: 4,
-      imgUrl: 'https://i.imgur.com/FPLfdoB.png',
-      title: 'Woolworths',
-      tags: ['A', 'B', 'C'],
-      desc: 'Bring a little good to everyone, everyday.'
-    }]
-    return { nearbyList }
-  }
+    name: 'NearbyShops',
+    setup() {
+        // const nearbyList = [{
+        //     id: 1,
+        //     imgUrl: 'https://i.imgur.com/LMzplaf.png',
+        //     title: 'Coles',
+        //     tags: ['A', 'B', 'C'],
+        //     desc: 'Good things are happening at Coles!'
+        // }, {
+        //     id: 2,
+        //     imgUrl: 'https://i.imgur.com/FPLfdoB.png',
+        //     title: 'Woolworths',
+        //     tags: ['A', 'B', 'C'],
+        //     desc: 'Bring a little good to everyone, everyday.'
+        // }, {
+        //     id: 3,
+        //     imgUrl: 'https://i.imgur.com/FPLfdoB.png',
+        //     title: 'Woolworths',
+        //     tags: ['A', 'B', 'C'],
+        //     desc: 'Bring a little good to everyone, everyday.'
+        // }, {
+        //     id: 4,
+        //     imgUrl: 'https://i.imgur.com/FPLfdoB.png',
+        //     title: 'Woolworths',
+        //     tags: ['A', 'B', 'C'],
+        //     desc: 'Bring a little good to everyone, everyday.'
+        // }]
+        // return { nearbyList }
+        const { nearbyList, getNearbyList } = useNearbyListEffect();
+        getNearbyList();
+        return { nearbyList };
+    }
 }
 </script>
 <style lang="scss" scoped>
