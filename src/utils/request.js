edd74df7d56e1145fd6fea07f72fe4a018d280b1
constcpt <51440152+constcpt@users.noreply.github.com>
2023-10-31 02:37:24 +1100

    部署上线修改URL

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
-  baseURL: "http://localhost:3000",
+  baseURL: "https://ecommercebackend-v3p3.onrender.com",
   withCredentials: true, // 允许跨域传递 cookie （登录）
   timeout: 10000,
 });
